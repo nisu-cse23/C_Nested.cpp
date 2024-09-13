6d031776a4536a9024770518bdf0fd595c450bf8
nisu-cse23 <160548482+nisu-cse23@users.noreply.github.com>
2024-09-13 21:32:51 +0600

    Add files via upload

---
+++ DisplayInvertedRightTriangle.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int row,col,n;
+    printf("Hoe many rows of right trainagle?");
+    scanf("%d",&n);
+
+    for(row=n;row>=1;row--){
+        for(col=1;col<=row;col++)
+        printf("*");
+        printf("\n");
+        
+    }
+    return 0;
+}
